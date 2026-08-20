c5bb2f11765bb57fe1fdd1088bee65fef6cc5962
ssdetlab <ssetlab@gmail.com>
2026-08-20 18:55:51 +0200

    optimize selection for the atlas cluster, update branches with angles

--- include/Analysis/Postprocessor.hpp
+++ include/Analysis/Postprocessor.hpp
@@ -16,9 +16,21 @@
 /// @brief class handling the E320 data postprocessing
 class Postprocessor {
  public:
-  /// Shorthand for the ROOT file handles
-  using FileHandle = std::tuple<TFile *, TChain *, TTree *>;
+  /// @brief File handle struct
+  struct FileHandle {
+    /// File pointer
+    TFile* file;
+    /// Chain owner pointer
+    TChain* chainOwner;
+    /// Tree storing the whole track data
+    TTree* dataTree;
+    /// Tree storing selection branches
+    TTree* selectionTree;
+    /// Ranges of the events
+    std::vector<std::pair<std::uint32_t, std::uint32_t>> eventRanges;
+  };
 
+  /// @brief Track handle struct
   struct TrackHandle {
     int treeIdx = -1;
     double chi2 = std::numeric_limits<double>::max();
@@ -59,7 +71,7 @@ class Postprocessor {
   /// @brief Run data preprocessing
   ///
   /// @param opt preprocessing options
-  void processFiles(const Options &opt);
+  void processFiles(const Options& opt);
 
  private:
   /// @brief collect run data paths for a collection of runs
@@ -67,7 +79,7 @@ class Postprocessor {
   /// @param opt preprocessing options
   ///
   /// @return map of run prefix to its path collection
-  std::vector<std::string> collectDataPaths(const Options &opt) const;
+  std::vector<std::string> collectDataPaths(const Options& opt) const;
 
   /// @brief get file handle for a given ROOT tree
   ///
@@ -75,50 +87,108 @@ class Postprocessor {
   /// @param paths run data paths
   ///
   /// @return file handle for run data reading
-  FileHandle getInFileHandle(const std::string &treeName,
-                             const std::vector<std::string> &paths);
-
-  std::vector<std::pair<std::uint32_t, std::uint32_t>> getEventRanges(
-      TTree *tree) const;
+  FileHandle getInFileHandle(const std::string& treeName,
+                             const std::vector<std::string>& paths);
 
  protected:
-  /// Event meta data
+  /// EUDAQ trigger ID
   std::size_t m_eudaqTrgN = 0;
+
+  /// EUDAQ DAQ run number
   std::size_t m_eudaqDAQNumber = 0;
+
+  /// EUDAQ DAQ run start timestamp
   std::size_t m_eudaqRunStartTs = 0;
+
+  /// EUDAQ DAQ run end timestamp
   std::size_t m_eudaqRunEndTs = 0;
+
+  /// Event EPICS parity
   std::size_t m_epicsParity = 0;
+
+  /// Event EPICS PID
   std::size_t m_epicsPulseId = 0;
+
+  /// Event EPICS DAQ number
   std::size_t m_epicsDAQNumber = 0;
 
-  /// Measurement hits
-  std::vector<TVector3> *m_trackHitsGlobal = nullptr;
-  std::vector<TVector2> *m_trackHitsLocal = nullptr;
+  /// Magnet configuration as seen by track
+  double m_quad1Grad = 0;
+  double m_quad2Grad = 0;
+  double m_quad3Grad = 0;
+  double m_xCorrectorStrength = 0;
+  double m_dipoleStrength = 0;
+
+  /// Measurement hits in the surface frame
+  std::vector<TVector2>* m_trackHitsLocal = nullptr;
+
+  /// Measurement hits in the global frame
+  std::vector<TVector3>* m_trackHitsGlobal = nullptr;
+
+  /// Direction measurements in the track frame
+  std::vector<TVector3>* m_onSurfaceTrackDirection = nullptr;
 
   /// Covariances of the track hits
-  std::vector<TMatrixD> *m_trackHitsCovs = nullptr;
+  std::vector<TMatrixD>* m_trackHitCovs = nullptr;
+
+  /// Covariances of the track agnles
+  std::vector<TMatrixD>* m_trackAngleCovs = nullptr;
 
   /// Geometry ids of the track hits
-  std::vector<std::size_t> *m_geometryIds = nullptr;
+  std::vector<std::size_t>* m_geometryIds = nullptr;
 
-  /// KF predicted track hits
-  std::vector<TVector3> *m_predictedTrackHitsGlobal = nullptr;
-  std::vector<TVector3> *m_filteredTrackHitsGlobal = nullptr;
-  std::vector<TVector3> *m_smoothedTrackHitsGlobal = nullptr;
+  /// KF predicted track hits in the surface frame
+  std::vector<TVector2>* m_predictedTrackHitsLocal = nullptr;
+  std::vector<TVector2>* m_filteredTrackHitsLocal = nullptr;
+  std::vector<TVector2>* m_smoothedTrackHitsLocal = nullptr;
 
-  std::vector<TVector2> *m_predictedTrackHitsLocal = nullptr;
-  std::vector<TVector2> *m_filteredTrackHitsLocal = nullptr;
-  std::vector<TVector2> *m_smoothedTrackHitsLocal = nullptr;
+  /// KF predicted track hits in the global frame
+  std::vector<TVector3>* m_predictedTrackHitsGlobal = nullptr;
+  std::vector<TVector3>* m_filteredTrackHitsGlobal = nullptr;
+  std::vector<TVector3>* m_smoothedTrackHitsGlobal = nullptr;
+
+  /// KF predicted on surface momenta in the track frame
+  std::vector<TLorentzVector>* m_predictedOnSurfaceMomentum = nullptr;
+  std::vector<TLorentzVector>* m_filteredOnSurfaceMomentum = nullptr;
+  std::vector<TLorentzVector>* m_smoothedOnSurfaceMomentum = nullptr;
 
   /// KF residuals with respect to the measurements
-  std::vector<TVector2> *m_predictedResiduals = nullptr;
-  std::vector<TVector2> *m_filteredResiduals = nullptr;
-  std::vector<TVector2> *m_smoothedResiduals = nullptr;
+  std::vector<TVector2>* m_predictedHitResiduals = nullptr;
+  std::vector<TVector2>* m_filteredHitResiduals = nullptr;
+  std::vector<TVector2>* m_smoothedHitResiduals = nullptr;
+
+  std::vector<TVector2>* m_predictedAngleResiduals = nullptr;
+  std::vector<TVector2>* m_filteredAngleResiduals = nullptr;
+  std::vector<TVector2>* m_smoothedAngleResiduals = nullptr;
 
   /// KF pulls with respect to the measurements
-  std::vector<TVector2> *m_predictedPulls = nullptr;
-  std::vector<TVector2> *m_filteredPulls = nullptr;
-  std::vector<TVector2> *m_smoothedPulls = nullptr;
+  std::vector<TVector2>* m_predictedHitPulls = nullptr;
+  std::vector<TVector2>* m_filteredHitPulls = nullptr;
+  std::vector<TVector2>* m_smoothedHitPulls = nullptr;
+
+  std::vector<TVector2>* m_predictedAnglePulls = nullptr;
+  std::vector<TVector2>* m_filteredAnglePulls = nullptr;
+  std::vector<TVector2>* m_smoothedAnglePulls = nullptr;
+
+  /// Guessed bound track parameters
+  TVectorD* m_boundTrackParametersGuess = nullptr;
+  TMatrixD* m_boundTrackCovGuess = nullptr;
+
+  /// KF predicted bound track parameters
+  TVectorD* m_boundTrackParametersEst = nullptr;
+  TMatrixD* m_boundTrackCovEst = nullptr;
+
+  /// Initial guess of the momentum at the IP
+  TLorentzVector* m_originMomentumGuess = nullptr;
+
+  /// Initial guess of the vertex at the IP
+  TVector3* m_vertexGuess = nullptr;
+
+  /// KF predicted momentum at the IP
+  TLorentzVector* m_originMomentumEst = nullptr;
+
+  /// KF predicted vertex at the IP
+  TVector3* m_vertexEst = nullptr;
 
   /// Chi2 of the track
   /// with respect ot the
@@ -131,10 +201,10 @@ class Postprocessor {
   /// of the track
   std::size_t m_ndf = 0;
 
-  /// TrackId
+  /// Track ID
   std::size_t m_trackId = 0;
 
-  /// EventId
+  /// Event ID
   std::size_t m_eventId = 0;
 
   /// PDG ID
@@ -142,24 +212,4 @@ class Postprocessor {
 
   /// Charge
   int m_charge = 0;
-
-  /// Guessed bound track parameters
-  TVectorD *m_boundTrackParametersGuess = nullptr;
-  TMatrixD *m_boundTrackCovGuess = nullptr;
-
-  /// KF predicted bound track parameters
-  TVectorD *m_boundTrackParametersEst = nullptr;
-  TMatrixD *m_boundTrackCovEst = nullptr;
-
-  /// Initial guess of the momentum at the IP
-  TLorentzVector *m_originMomentumGuess = nullptr;
-
-  /// Initial guess of the vertex at the IP
-  TVector3 *m_vertexGuess = nullptr;
-
-  /// KF predicted momentum at the IP
-  TLorentzVector *m_originMomentumEst = nullptr;
-
-  /// KF predicted vertex at the IP
-  TVector3 *m_vertexEst = nullptr;
 };
--- src/Analysis/Postprocessor.cpp
+++ src/Analysis/Postprocessor.cpp
@@ -43,117 +43,37 @@ std::vector<std::string> Postprocessor::collectDataPaths(
 Postprocessor::FileHandle Postprocessor::getInFileHandle(
     const std::string &treeName, const std::vector<std::string> &paths) {
   TFile *file = nullptr;
-  TTree *tree = nullptr;
+  TTree *dataTree = nullptr;
+  TTree *selectionTree = nullptr;
   TChain *chainOwner = new TChain(treeName.c_str(), treeName.c_str());
   if (paths.size() == 1) {
     file = new TFile(paths.at(0).c_str());
-    tree = file->Get<TTree>(treeName.c_str());
+    dataTree = file->Get<TTree>(treeName.c_str());
+    selectionTree = file->Get<TTree>(treeName.c_str());
   } else {
     chainOwner = new TChain(treeName.c_str());
     for (const auto &path : paths) {
       chainOwner->Add(path.c_str());
     }
-    tree = dynamic_cast<TTree *>(chainOwner);
+    dataTree = dynamic_cast<TTree *>(chainOwner);
+    selectionTree = dynamic_cast<TTree *>(chainOwner);
   }
 
-  // Event meta data
-  tree->SetBranchAddress("eudaqTrgN", &m_eudaqTrgN);
-  tree->SetBranchAddress("eudaqDAQNumber", &m_eudaqDAQNumber);
-  tree->SetBranchAddress("eudaqRunStartTs", &m_eudaqRunStartTs);
-  tree->SetBranchAddress("eudaqRunEndTs", &m_eudaqRunEndTs);
-  tree->SetBranchAddress("epicsParity", &m_epicsParity);
-  tree->SetBranchAddress("epicsPulseId", &m_epicsPulseId);
-  tree->SetBranchAddress("epicsDAQNumber", &m_epicsDAQNumber);
+  // Set event ID branch
+  dataTree->SetBranchAddress("eventId", &m_eventId);
+  if (dataTree->GetBranch("eventId") == nullptr) {
+    throw std::invalid_argument("Missing eventId branch");
+  }
+  auto nEntries = static_cast<std::size_t>(dataTree->GetEntries());
 
-  // Measurement hits
-  tree->SetBranchAddress("trackHitsGlobal", &m_trackHitsGlobal);
-  tree->SetBranchAddress("trackHitsLocal", &m_trackHitsLocal);
-
-  // Covariances of the track hits
-  tree->SetBranchAddress("trackHitsCovs", &m_trackHitsCovs);
-
-  // Geometry ids of the track hits
-  tree->SetBranchAddress("geometryIds", &m_geometryIds);
-
-  // KF predicted track hits
-  tree->SetBranchAddress("predictedTrackHitsGlobal",
-                         &m_predictedTrackHitsGlobal);
-  tree->SetBranchAddress("filteredTrackHitsGlobal", &m_filteredTrackHitsGlobal);
-  tree->SetBranchAddress("smoothedTrackHitsGlobal", &m_smoothedTrackHitsGlobal);
-
-  tree->SetBranchAddress("predictedTrackHitsLocal", &m_predictedTrackHitsLocal);
-  tree->SetBranchAddress("filteredTrackHitsLocal", &m_filteredTrackHitsLocal);
-  tree->SetBranchAddress("smoothedTrackHitsLocal", &m_smoothedTrackHitsLocal);
-
-  // KF residuals with respect to the measurements
-  tree->SetBranchAddress("predictedResiduals", &m_predictedResiduals);
-  tree->SetBranchAddress("filteredResiduals", &m_filteredResiduals);
-  tree->SetBranchAddress("smoothedResiduals", &m_smoothedResiduals);
-
-  // KF pulls with respect to the measurements
-  tree->SetBranchAddress("predictedPulls", &m_predictedPulls);
-  tree->SetBranchAddress("filteredPulls", &m_filteredPulls);
-  tree->SetBranchAddress("smoothedPulls", &m_smoothedPulls);
-
-  // Initial guess of the momentum at the IP
-  tree->SetBranchAddress("originMomentumGuess", &m_originMomentumGuess);
-  tree->SetBranchAddress("vertexGuess", &m_vertexGuess);
-
-  /// Guessed bound track parameters
-  tree->SetBranchAddress("boundTrackParametersGuess",
-                         &m_boundTrackParametersGuess);
-  tree->SetBranchAddress("boundTrackCovGuess", &m_boundTrackCovGuess);
-
-  /// KF predicted bound track parameters
-  tree->SetBranchAddress("boundTrackParametersEst", &m_boundTrackParametersEst);
-  tree->SetBranchAddress("boundTrackCovEst", &m_boundTrackCovEst);
-
-  /// Initial guess of the momentum at the IP
-  tree->SetBranchAddress("originMomentumGuess", &m_originMomentumGuess);
-
-  /// Initial guess of the vertex at the IP
-  tree->SetBranchAddress("vertexGuess", &m_vertexGuess);
-
-  /// KF predicted momentum at the IP
-  tree->SetBranchAddress("originMomentumEst", &m_originMomentumEst);
-
-  /// KF predicted vertex at the IP
-  tree->SetBranchAddress("vertexEst", &m_vertexEst);
-
-  // Chi2 and ndf of the fitted track
-  tree->SetBranchAddress("chi2Predicted", &m_chi2Predicted);
-  tree->SetBranchAddress("chi2Filtered", &m_chi2Filtered);
-  tree->SetBranchAddress("chi2Smoothed", &m_chi2Smoothed);
-  tree->SetBranchAddress("ndf", &m_ndf);
-
-  // Track ID
-  tree->SetBranchAddress("trackId", &m_trackId);
-
-  // Event ID
-  tree->SetBranchAddress("eventId", &m_eventId);
-
-  // PDG ID
-  tree->SetBranchAddress("pdgId", &m_pdgId);
-
-  // Charge
-  tree->SetBranchAddress("charge", &m_charge);
-
-  return {file, chainOwner, tree};
-}
-
-std::vector<std::pair<std::uint32_t, std::uint32_t>>
-Postprocessor::getEventRanges(TTree *tree) const {
+  // Get event ranges
   std::cout << "Getting event ranges\n";
   std::vector<std::pair<std::uint32_t, std::uint32_t>> eventRanges;
-
-  tree->SetBranchStatus("*", false);
-  tree->SetBranchStatus("eventId", true);
-  tree->GetEntry(0);
+  dataTree->GetEntry(0);
   std::size_t currentEventId = m_eventId;
   std::size_t currentIdx = 0;
-  std::size_t nEntries = tree->GetEntries();
-  for (std::size_t i = 1; i < nEntries; i++) {
-    tree->GetEntry(i);
+  for (std::size_t i = 0; i < nEntries; i++) {
+    dataTree->GetEntry(i);
     if (m_eventId != currentEventId || i == nEntries - 1) {
       if (i != nEntries - 1) {
         eventRanges.push_back({currentIdx, i});
@@ -166,27 +86,147 @@ Postprocessor::getEventRanges(TTree *tree) const {
       }
     }
   }
-  tree->SetBranchStatus("*", true);
-
   std::cout << "Total ranges found " << eventRanges.size() << "\n";
   std::cout << "\n\n\n\n";
-  return eventRanges;
+
+  // Event meta data
+  dataTree->SetBranchAddress("eudaqTrgN", &m_eudaqTrgN);
+  dataTree->SetBranchAddress("eudaqDAQNumber", &m_eudaqDAQNumber);
+  dataTree->SetBranchAddress("eudaqRunStartTs", &m_eudaqRunStartTs);
+  dataTree->SetBranchAddress("eudaqRunEndTs", &m_eudaqRunEndTs);
+  dataTree->SetBranchAddress("epicsParity", &m_epicsParity);
+  dataTree->SetBranchAddress("epicsPulseId", &m_epicsPulseId);
+  dataTree->SetBranchAddress("epicsDAQNumber", &m_epicsDAQNumber);
+
+  // Magnet configuration as seen by track
+  dataTree->SetBranchAddress("quad1Grad", &m_quad1Grad);
+  dataTree->SetBranchAddress("quad2Grad", &m_quad2Grad);
+  dataTree->SetBranchAddress("quad3Grad", &m_quad3Grad);
+  dataTree->SetBranchAddress("xCorrectorStrength", &m_xCorrectorStrength);
+  dataTree->SetBranchAddress("dipoleStrength", &m_dipoleStrength);
+
+  // Measurement hits in the surface frame
+  dataTree->SetBranchAddress("trackHitsLocal", &m_trackHitsLocal);
+
+  // Measurement hits in the global frame
+  dataTree->SetBranchAddress("trackHitsGlobal", &m_trackHitsGlobal);
+
+  // Direction measurements in the track frame
+  dataTree->SetBranchAddress("onSurfaceTrackDirection",
+                             &m_onSurfaceTrackDirection);
+
+  // Covariances of the track hits
+  dataTree->SetBranchAddress("trackHitCovs", &m_trackHitCovs);
+
+  // Covariances of the track agnles
+  dataTree->SetBranchAddress("trackAngleCovs", &m_trackAngleCovs);
+
+  // Geometry ids of the track hits
+  dataTree->SetBranchAddress("geometryIds", &m_geometryIds);
+
+  // KF predicted track hits in the surface frame
+  dataTree->SetBranchAddress("predictedTrackHitsLocal",
+                             &m_predictedTrackHitsLocal);
+  dataTree->SetBranchAddress("filteredTrackHitsLocal",
+                             &m_filteredTrackHitsLocal);
+  dataTree->SetBranchAddress("smoothedTrackHitsLocal",
+                             &m_smoothedTrackHitsLocal);
+
+  // KF predicted track hits in the global frame
+  dataTree->SetBranchAddress("predictedTrackHitsGlobal",
+                             &m_predictedTrackHitsGlobal);
+  dataTree->SetBranchAddress("filteredTrackHitsGlobal",
+                             &m_filteredTrackHitsGlobal);
+  dataTree->SetBranchAddress("smoothedTrackHitsGlobal",
+                             &m_smoothedTrackHitsGlobal);
+
+  // KF predicted on surface momenta in the track frame
+  dataTree->SetBranchAddress("predictedOnSurfaceMomentum",
+                             &m_predictedOnSurfaceMomentum);
+  dataTree->SetBranchAddress("filteredOnSurfaceMomentum",
+                             &m_filteredOnSurfaceMomentum);
+  dataTree->SetBranchAddress("smoothedOnSurfaceMomentum",
+                             &m_smoothedOnSurfaceMomentum);
+
+  // KF residuals with respect to the measurements
+  dataTree->SetBranchAddress("predictedHitResiduals", &m_predictedHitResiduals);
+  dataTree->SetBranchAddress("filteredHitResiduals", &m_filteredHitResiduals);
+  dataTree->SetBranchAddress("smoothedHitResiduals", &m_smoothedHitResiduals);
+
+  dataTree->SetBranchAddress("predictedAngleResiduals",
+                             &m_predictedAngleResiduals);
+  dataTree->SetBranchAddress("filteredAngleResiduals",
+                             &m_filteredAngleResiduals);
+  dataTree->SetBranchAddress("smoothedAngleResiduals",
+                             &m_smoothedAngleResiduals);
+
+  // KF pulls with respect to the measurements
+  dataTree->SetBranchAddress("predictedHitPulls", &m_predictedHitPulls);
+  dataTree->SetBranchAddress("filteredHitPulls", &m_filteredHitPulls);
+  dataTree->SetBranchAddress("smoothedHitPulls", &m_smoothedHitPulls);
+
+  dataTree->SetBranchAddress("predictedAnglePulls", &m_predictedAnglePulls);
+  dataTree->SetBranchAddress("filteredAnglePulls", &m_filteredAnglePulls);
+  dataTree->SetBranchAddress("smoothedAnglePulls", &m_smoothedAnglePulls);
+
+  // Guessed bound track parameters
+  dataTree->SetBranchAddress("boundTrackParametersGuess",
+                             &m_boundTrackParametersGuess);
+  dataTree->SetBranchAddress("boundTrackCovGuess", &m_boundTrackCovGuess);
+
+  // KF predicted bound track parameters
+  dataTree->SetBranchAddress("boundTrackParametersEst",
+                             &m_boundTrackParametersEst);
+  dataTree->SetBranchAddress("boundTrackCovEst", &m_boundTrackCovEst);
+
+  // Initial guess of the momentum at the IP
+  dataTree->SetBranchAddress("originMomentumGuess", &m_originMomentumGuess);
+
+  // Initial guess of the vertex at the IP
+  dataTree->SetBranchAddress("vertexGuess", &m_vertexGuess);
+
+  // KF predicted momentum at the IP
+  dataTree->SetBranchAddress("originMomentumEst", &m_originMomentumEst);
+
+  // KF predicted vertex at the IP
+  dataTree->SetBranchAddress("vertexEst", &m_vertexEst);
+
+  // Chi2 of the track with respect ot the measurement
+  dataTree->SetBranchAddress("chi2Predicted", &m_chi2Predicted);
+  dataTree->SetBranchAddress("chi2Filtered", &m_chi2Filtered);
+  dataTree->SetBranchAddress("chi2Smoothed", &m_chi2Smoothed);
+
+  // Number of degrees of freedom of the track
+  dataTree->SetBranchAddress("ndf", &m_ndf);
+
+  // Track ID
+  dataTree->SetBranchAddress("trackId", &m_trackId);
+
+  // PDG ID
+  dataTree->SetBranchAddress("pdgId", &m_pdgId);
+
+  // Charge
+  dataTree->SetBranchAddress("charge", &m_charge);
+
+  // Set selection tree branches
+  selectionTree->SetBranchAddress("trackHitsGlobal", &m_trackHitsGlobal);
+  selectionTree->SetBranchAddress("chi2Smoothed", &m_chi2Smoothed);
+
+  return {file, chainOwner, dataTree, selectionTree, eventRanges};
 }
 
 void Postprocessor::processFiles(const Options &opt) {
   auto paths = collectDataPaths(opt);
 
   // Get data tree
-  auto [inFile, inChain, inTree] = getInFileHandle(opt.inDataTreeName, paths);
+  auto [inFile, inChain, dataTree, selectionTree, eventRanges] =
+      getInFileHandle(opt.inDataTreeName, paths);
 
   // Initilize output file
   TFile *outFile = new TFile(opt.outDataPath.c_str(), "RECREATE");
   TTree *outTree = nullptr;
-  outTree = inTree->CloneTree(0);
-  inTree->CopyAddresses(outTree);
-
-  // Get event ranges
-  auto eventRanges = getEventRanges(inTree);
+  outTree = dataTree->CloneTree(0);
+  dataTree->CopyAddresses(outTree);
 
   // Process events
   std::size_t nRanges = eventRanges.size();
@@ -194,10 +234,6 @@ void Postprocessor::processFiles(const Options &opt) {
   std::size_t endIdx = std::min(opt.skip + opt.events, nRanges);
   for (std::size_t i = startIdx; i < endIdx; i++) {
     auto [eventStartIdx, eventEndIdx] = eventRanges.at(i);
-
-    inTree->SetBranchStatus("*", false);
-    inTree->SetBranchStatus("trackHitsGlobal", true);
-    inTree->SetBranchStatus("chi2Smoothed", true);
 
     // Go over the hits and indetify the best chi2
     // track passing through each of them
@@ -207,7 +243,7 @@ void Postprocessor::processFiles(const Options &opt) {
         clusterMap;
     clusterMap.reserve((eventEndIdx - eventStartIdx) * 5);
     for (int j = eventStartIdx; j < eventEndIdx; j++) {
-      inTree->GetEntry(j);
+      selectionTree->GetEntry(j);
       for (const auto &hit : *m_trackHitsGlobal) {
         auto &mapEntry = clusterMap[hit];
         if (mapEntry.chi2 > m_chi2Smoothed) {
@@ -218,7 +254,6 @@ void Postprocessor::processFiles(const Options &opt) {
         }
       }
     }
-    inTree->SetBranchStatus("*", true);
 
     // Get the unique track indices
     std::set<int> trackIdxs;
@@ -231,7 +266,7 @@ void Postprocessor::processFiles(const Options &opt) {
     }
     // TTree IO is faster with sequential readout
     for (auto j : trackIdxs) {
-      inTree->GetEntry(j);
+      dataTree->GetEntry(j);
       outTree->Fill();
     }
     if (i % 10 == 0) {
